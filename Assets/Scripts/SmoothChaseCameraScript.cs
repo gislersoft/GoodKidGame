be556b87133ea508359578f50e5cd6105ff3c3eb
Gisler Garces <gislersoft@hotmail.com>
2022-01-26 20:51:05 -0500

    Intentando solucionar oclusion

--- Assets/Scripts/SmoothChaseCameraScript.cs
+++ Assets/Scripts/SmoothChaseCameraScript.cs
@@ -10,6 +10,8 @@ public class SmoothChaseCameraScript : MonoBehaviour
     public float SmoothTime = 0.3f;
 
     private Vector3 velocity = Vector3.zero;
+
+    private RaycastHit HitInfo;
  
     private void Start()
     {
@@ -19,7 +21,22 @@ public class SmoothChaseCameraScript : MonoBehaviour
     private void LateUpdate()
     {
         Vector3 targetPosition = Target.position + Offset;
-        this.transform.position = Vector3.SmoothDamp(this.transform.position, targetPosition, ref velocity, SmoothTime);
+        float distanceFix = 0;
+        Transform cameraTransform = Camera.main.transform;
+        Vector3 rayDirection = cameraTransform.position - Target.position;
+        /*
+        if (Physics.Raycast(Target.position,rayDirection.normalized, out HitInfo, 100.0f)) {
+            Debug.DrawRay(Target.position,rayDirection.normalized * 100.0f, Color.yellow);
+            if (HitInfo.transform != null) {
+                targetPosition = HitInfo.point;
+            }
+        }
+        */
+
+        this.transform.position = Vector3.SmoothDamp(
+            this.transform.position,
+            targetPosition,
+            ref velocity, 0.0f);
  
         transform.LookAt(Target);
     }
