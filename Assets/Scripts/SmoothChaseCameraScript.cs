9c44a1d0003e40c40da242387e89d2896779244c
Gisler Garces <gislersoft@hotmail.com>
2022-01-26 20:13:22 -0500

    Implementando movimiento de la camara

---
+++ Assets/Scripts/SmoothChaseCameraScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothChaseCameraScript : MonoBehaviour
+{
+
+    public Transform Target;
+    public Vector3 Offset;
+    public float SmoothTime = 0.3f;
+
+    private Vector3 velocity = Vector3.zero;
+ 
+    private void Start()
+    {
+        Offset = this.transform.position - Target.position;
+    }
+ 
+    private void LateUpdate()
+    {
+        Vector3 targetPosition = Target.position + Offset;
+        this.transform.position = Vector3.SmoothDamp(this.transform.position, targetPosition, ref velocity, SmoothTime);
+ 
+        transform.LookAt(Target);
+    }
+}
