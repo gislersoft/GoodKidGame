59bf2633a4e1bf7ba8b7a01efe6d2cb6f5278177
Gisler Garces <gislersoft@hotmail.com>
2022-01-26 19:51:55 -0500

    Creando un cubo que se pueda mover con las teclas

--- Assets/Scripts/CharacterControllerScript.cs
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -13,7 +13,7 @@ public class CharacterControllerScript : MonoBehaviour
 
     private void Start()
     {
-        controller = gameObject.AddComponent<CharacterController>();
+        controller = gameObject.GetComponent<CharacterController>();
     }
 
     void Update()
