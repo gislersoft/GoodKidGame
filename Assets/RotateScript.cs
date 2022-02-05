6d02d4e391c84e98663c7f5f4c06e5555bc3fea0
Gisler Garces <gislersoft@hotmail.com>
2022-02-04 19:10:51 -0500

    agregando notas musicales

---
+++ Assets/RotateScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScript : MonoBehaviour
+{
+    public float speed = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(0, speed, 0, Space.Self);
+    }
+}
