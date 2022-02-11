f027a52efcb5d171b94da7fab8f56e2de5a703d4
Gisler Garces <gislersoft@hotmail.com>
2022-02-11 18:58:40 -0500

    Cambios durante la clase de audio y UI

---
+++ Assets/Scripts/NotaMusical.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotaMusical : MonoBehaviour
+{
+    public AudioSource fuenteAudio;
+    public AudioClip sonidoNota;
+    public int valor = 50;
+
+    private Text textScoreRef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textScoreRef = GameObject.Find("score").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            fuenteAudio.PlayOneShot(sonidoNota);
+            int total = (int.Parse(textScoreRef.text) + valor);
+            Debug.Log(total);
+            textScoreRef.text = total + "";
+            Destroy(gameObject);
+        }
+    }
+}
+
